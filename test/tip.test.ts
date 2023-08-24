d89ebd5336f085552fc5f6bd9e38135032a400bd
KyleTryon <Kyle.Tryon@circleci.com>
2023-08-24 12:26:47 -0400

    feat: add tests

---
+++ test/tip.test.ts
@@ -0,0 +1,19 @@
+import { expect, test } from "vitest";
+import fs from "fs";
+import path from "path";
+
+const contentPath = 'src/content/'
+
+test("Validate Tip File Names", async () => {
+  const idPattern = /^[a-z0-9_]*\.md$/;
+  const errors: string[] = [];
+  console.log(`Attempting to read from: ${path.resolve(contentPath + "tips")}`);
+  const filenames = fs.readdirSync(contentPath + 'tips');
+  filenames.forEach(filename => {
+    if (!idPattern.test(filename)) {
+      errors.push(`Invalid filename: ${filename} :: Use only lowercase letters, numbers, and underscores.`);
+    }
+  });
+  const errorMsg = errors.join("\n");
+  expect(errors, errorMsg).toEqual([]);
+});
