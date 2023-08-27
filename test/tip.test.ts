9cdc321706a2a469f2329a666daad3de17d89ba0
KyleTryon <Kyle.Tryon@circleci.com>
2023-08-26 22:10:08 -0400

    feat!: enforce url friendly config filenames

--- test/tip.test.ts
+++ test/tip.test.ts
@@ -4,16 +4,34 @@ import path from "path";
 
 const contentPath = "src/content/";
 // This sets the URL pattern by enforcing filenames.
-const idPattern = /^(?!.*--)[a-z0-9](?:[a-z0-9-]{3,}[a-z0-9])+\.md$/;
+const tipIdPattern = /^(?!.*--)[a-z0-9](?:[a-z0-9-]{3,}[a-z0-9])+\.md$/;
+const configIdPattern = /^(?!.*--)[a-z](?:[a-z0-9-]*?[a-z0-9])*\.yaml$/;
+const tipFiles = fs.readdirSync(contentPath + "tips");
+const configKindFiles = fs.readdirSync(contentPath + "configKinds");
 
 test("Validate Tip File Names", async () => {
   const errors: string[] = [];
   console.log(`Attempting to read from: ${path.resolve(contentPath + "tips")}`);
-  const filenames = fs.readdirSync(contentPath + "tips");
-  filenames.forEach((filename) => {
-    if (!idPattern.test(filename)) {
+  tipFiles.forEach((filename) => {
+    if (!tipIdPattern.test(filename)) {
       errors.push(
-        `Invalid filename: ${filename} :: Use only lowercase letters, numbers, and underscores.`,
+        `Invalid filename: ${filename} :: Use only lowercase letters, numbers, and dashes.`
+      );
+    }
+  });
+  const errorMsg = errors.join("\n");
+  expect(errors, errorMsg).toEqual([]);
+});
+
+test("Validate ConfigKind File Names", async () => {
+  const errors: string[] = [];
+  console.log(
+    `Attempting to read from: ${path.resolve(contentPath + "configKinds")}`
+  );
+  configKindFiles.forEach((filename) => {
+    if (!configIdPattern.test(filename)) {
+      errors.push(
+        `Invalid filename: ${filename} :: Use only lowercase letters, numbers, and dashes.`
       );
     }
   });
