fbba1370a227a42409343712a706c14e44f74f92
KyleTryon <Kyle.Tryon@circleci.com>
2023-08-30 22:13:18 -0400

    feat: implement react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://config.tips",
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()]
 });
